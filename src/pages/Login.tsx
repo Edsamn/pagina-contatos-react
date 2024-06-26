32f71dfd8938905267fa95721293ea0a41180e0e
Edsamn <eduardomsribas@gmail.comm>
2024-06-26 18:27:21 -0300

    feat: styles

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import Links from "../components/Links";
+import {Link} from "react-router-dom";
 
 const Login: React.FC = () => {
   const [name, setName] = useState<string>("");
@@ -7,23 +7,31 @@ const Login: React.FC = () => {
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log("Login bem-sucedido.");
-    setName("");
-    setEmail("");
+    if (name === "" || email === "") {
+      console.log("Por favor, preencha todos os campos.");
+    } else {
+      console.log("Login bem-sucedido.");
+      setName("");
+      setEmail("");
+    }
   }
 
   return (
     <>
       <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} style={{display: "flex", flexDirection: "column"}}>
         <label htmlFor="name">Nome:</label>
         <input type="text" value={name} onChange={(event) => setName(event.target.value)} />
         <br />
         <label htmlFor="email">E-mail:</label>
         <input type="text" value={email} onChange={(event) => setEmail(event.target.value)} />
-        <button type="submit">Enviar</button>
+        <button type="submit" style={{backgroundColor: "#2f14e1", marginTop: "10px"}}>
+          Enviar
+        </button>
+        <small>
+          Não possui conta? <Link to="/">Cadastrar</Link>
+        </small>
       </form>
-      <Links />
     </>
   );
 };
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -8,7 +8,7 @@ const router = createBrowserRouter([
     element: <Cadastro />,
   },
   {
-    path: "/contato",
+    path: "/login",
     element: <Login />,
   },
 ]);
